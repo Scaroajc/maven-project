247b14d336597096cc2d1762b603c2ef30e524f6
Scaroajc <47109335+Scaroajc@users.noreply.github.com>
2019-01-29 10:29:17 +0100

    Update Greeter.java
    Ajout des commentaires

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,21 @@
 package com.example;
 
-
+/**
+* Ceci est une class
+*/
 public class Greeter {
 
-  
+  /**
+  * Ceci est un constructeur
+  */
   public Greeter() {
 
   }
 
+  /**
+  * @param someone = nom de la personne
+  * @return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello Oscar, %s!", someone);
   }
